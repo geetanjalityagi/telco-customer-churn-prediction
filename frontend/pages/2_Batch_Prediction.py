063025dafee105f5554081ac238fabf66a7a9014
geetanjalityagi <geetanjalityagi1057@gmail.com>
2026-07-22 23:16:52 +0530

    STart Batch Prediction page

--- frontend/pages/2_Batch_Prediction.py
+++ frontend/pages/2_Batch_Prediction.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Batch Prediction", page_icon="📂", layout="wide
 render_sidebar("Batch Prediction")
 
 uploaded_file = st.file_uploader(
-    label = "Drag and drop CSV or Excel",
-    type=["csv", "xlsx"],
+    label = "Drag and drop CSV",
+    type=["csv"],
     help="Upload a CSV file containing customer information for batch churn prediction."
 )
